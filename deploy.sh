3ca784cab5b8c7a15112a3cf68d123de084c89fd
root <habdig7oficial>
2022-03-25 16:48:47 -0300

    auto deploy

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Copyright © 2022 Mateus Felipe da Silveira Vieira 
+
+
+#Este arquivo é parte do programa HGH-blog
+
+#HGH-blog é um software livre; você pode redistribuí-lo e/ou
+#modificá-lo dentro dos termos da Licença Pública Geral Affero GNU como
+#publicada pela Free Software Foundation (FSF); na versão 3 da
+#Licença, ou qualquer versão posterior.
+
+#HGH-blog é distribuído na esperança de que possa ser útil,
+#mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+#a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+#Licença Pública Geral Affero GNU para maiores detalhes.
+
+#Você deve ter recebido uma cópia da Licença Pública Geral Affero GNU junto
+#com este programa, Se não, veja <http://www.gnu.org/licenses/>.
+
+
+git add .
+
+git status
+
+git commit -m "auto deploy"
+
+git config --global user.email "habdig7@gmail.com"
+
+git push https://github.com/habdig7oficial/ac1-ti.git
